476ba38105907d500a96c53f39b88c836ba9e690
hakim <hakim@freebsd.hakim.com>
2014-08-13 20:09:21 +0100

    - open image & save it filtered

--- imageprocessing.py
+++ imageprocessing.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import wx
+from PIL import Image
+import os
 
 
 class MyFrame(wx.Frame):
@@ -7,6 +9,7 @@ class MyFrame(wx.Frame):
         # main frame
         wx.Frame.__init__(self, parent, id, title)
         self.Centre()
+        self.Maximize(True)
 
         # main layout
         self.mainbox = wx.BoxSizer(wx.VERTICAL)
@@ -17,6 +20,13 @@ class MyFrame(wx.Frame):
 
         # build toolbar
         self.BuildToolBar()
+
+        # init images
+        self.image = None
+        self.filteredimage = None
+
+        # add images containers
+        self.AddImagesContainers()
 
         # add statusbar
         self.CreateStatusBar()
@@ -90,18 +100,70 @@ class MyFrame(wx.Frame):
 
         # render & add to layout
         toolbar.Realize()
-        self.mainbox.Add(toolbar, 0, wx.EXPAND, 0, wx.EXPAND)
+        self.mainbox.Add(toolbar, 0, wx.EXPAND)
+
+    def AddImagesContainers(self):
+        """ Add image & filtered image containers
+        """
+        imagesbox = wx.BoxSizer(wx.HORIZONTAL)
+
+        # add image container to layout
+        imagesizer = wx.BoxSizer(wx.VERTICAL)
+        self.imagecontainer = wx.StaticBitmap(self)
+        imagesizer.Add(wx.StaticText(self, -1, label="Original image"), 0, wx.ALIGN_LEFT)
+        imagesizer.Add(self.imagecontainer, 1, wx.ALIGN_LEFT)
+        imagesbox.Add(imagesizer, 1, wx.ALIGN_LEFT)
+
+        # add a separator
+        separator = wx.StaticLine(self, -1, size=(0, 1000), style=wx.LI_VERTICAL)
+        imagesbox.Add(separator, 0)
+
+        # add filtered image container to layout
+        filteredimagesizer = wx.BoxSizer(wx.VERTICAL)
+        self.filteredimagecontainer = wx.StaticBitmap(self)
+        filteredimagesizer.Add(wx.StaticText(self, -1, label="Filtered image"), 0, wx.ALIGN_LEFT)
+        filteredimagesizer.Add(self.filteredimagecontainer, 1, wx.ALIGN_LEFT)
+        imagesbox.Add(filteredimagesizer, 1, wx.ALIGN_LEFT)
+
+        # add images layout to main layout
+        self.mainbox.Add(imagesbox, 0, wx.EXPAND)
 
     def OnOpen(self, event):
         """ Called when clicking on 'open' menu-item
             to open a file dialog
         """
-        pass
+        dlg = wx.FileDialog(self, "Choose an image", "", "", "Image files (*jpg,*.png)|*.jpeg;*.png", wx.OPEN)
+
+        # load selected image
+        if dlg.ShowModal() == wx.ID_OK:
+            path = dlg.GetPath()
+            self.image = Image.open(path)
+
+            # show image
+            self.imagecontainer.SetBitmap(wx.Bitmap(path))
+
+            # get filename & file extension
+            self.filename = os.path.basename(path)
+            self.fileextension = os.path.splitext(path)[1]
+
+            # change frame title
+            self.SetTitle(path + " - Image Processing")
+
+        dlg.Destroy()
 
     def OnSave(self, event):
         """ Called when clicking on 'save' menu-item
         """
-        pass
+        if not(self.filteredimage is None):
+            dlg = wx.FileDialog(self, "Save the image", "", "", "Image fileds (*.jpg,*png)|*.jpeg;*.png", wx.SAVE)
+
+            # save filtered image
+            if dlg.ShowModal() == wx.ID_OK:
+                self.filteredimage.save(dlg.GetPath() + self.fileextension)
+
+            dlg.Destroy()
+        else:
+            wx.MessageBox("Image not filtered yet.", "Error", style=wx.ICON_ERROR)
 
     def OnQuit(self, event):
         """ Called when clicking on 'quit' menu-item
@@ -112,13 +174,23 @@ class MyFrame(wx.Frame):
     def OnBlackAndWhite(self, event):
         """ Called when clicking on 'black and white' menu-item
         """
-        pass
+        if not(self.image is None):
+            self.filteredimage = self.image.convert('L')
+
+            # save a temporary copy of filtered image
+            tmpfilepath = '/tmp/' + self.filename
+            self.filteredimage.save(tmpfilepath)
+
+            # show filtered image
+            self.filteredimagecontainer.SetBitmap(wx.Bitmap(tmpfilepath))
+        else:
+            wx.MessageBox("Image not opened yet.", "Error", style=wx.ICON_ERROR)
 
     def OnAbout(self, event):
         """ Called when clicking on 'about' menu-item
             to show a message box dialog
         """
-        wx.MessageBox('Create by "h4k1m"')
+        wx.MessageBox("Create by 'h4k1m'")
 
 
 class MyApp(wx.App):
